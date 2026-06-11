ff808ee49ca4de9e854bfb035e81bf4fd722dfff
TharunKumar31 <tharunkumarsakthi2004@gmail.com>
2026-06-11 09:38:02 +0530

    Fixed build issues

--- src/Components/Shop/shop.js
+++ src/Components/Shop/shop.js
@@ -14,7 +14,7 @@ export function Shop() {
                             <div className="row">
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://media.istockphoto.com/id/177012688/photo/eggs-clipping-path.jpg?s=612x612&w=0&k=20&c=6efzckLd2rwFb1vRx257sub2zh8dUu5EaL7G0tM9nQ0=" className="img-fluid product-image" id="image" />
+                                        <img src="https://media.istockphoto.com/id/177012688/photo/eggs-clipping-path.jpg?s=612x612&w=0&k=20&c=6efzckLd2rwFb1vRx257sub2zh8dUu5EaL7G0tM9nQ0=" alt="product1"  className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Egg</h5>
                                         <p className="text-center"><del>$9.99</del>$8.99</p>
                                         <form className="text-center">
@@ -26,7 +26,7 @@ export function Shop() {
 
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://exat8rt6fi5.exactdn.com/wp-content/uploads/2022/05/carrot-01.jpg?strip=all&lossy=1&ssl=1" className="img-fluid product-image" id="image" />
+                                        <img src="https://exat8rt6fi5.exactdn.com/wp-content/uploads/2022/05/carrot-01.jpg?strip=all&lossy=1&ssl=1" alt="product2" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Carrot</h5>
                                         <p className="text-center"><del>$5.99</del>$4.99</p>
                                         <form className="text-center">
@@ -38,7 +38,7 @@ export function Shop() {
 
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://urjaseeds.com/cdn/shop/products/cucumber-seeds-malaysia-t103m_900x.jpg?v=1591175147" className="img-fluid product-image" id="image" />
+                                        <img src="https://urjaseeds.com/cdn/shop/products/cucumber-seeds-malaysia-t103m_900x.jpg?v=1591175147" alt="product3" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Cucumber</h5>
                                         <p className="text-center"><del>$8.99</del>$7.99</p>
                                         <form className="text-center">
@@ -50,7 +50,7 @@ export function Shop() {
 
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://www.bobtailfruit.co.uk/media/mageplaza/blog/post/4/2/42e9as7nataai4a6jcufwg.jpeg" className="img-fluid product-image" id="image" />
+                                        <img src="https://www.bobtailfruit.co.uk/media/mageplaza/blog/post/4/2/42e9as7nataai4a6jcufwg.jpeg" alt="product4" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Banana</h5>
                                         <p className="text-center"><del>$10.99</del>$9.99</p>
                                         <form className="text-center">
@@ -63,7 +63,7 @@ export function Shop() {
                             <div className="row">
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://iranfreshfruit.net/wp-content/uploads/2020/01/red-apple-fruit.jpg" className="img-fluid product-image" id="image" />
+                                        <img src="https://thumbs.dreamstime.com/b/red-apple-isolated-clipping-path-19130134.jpg" alt="product5" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Apple</h5>
                                         <p className="text-center"><del>$9.99</del>$8.99</p>
                                         <form className="text-center">
@@ -75,7 +75,7 @@ export function Shop() {
 
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://fairo.pk/wp-content/uploads/2019/04/Untitled-design-2025-02-26T094743.952.png" className="img-fluid product-image" id="image" />
+                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSph7KkV8WkmWuc6b4DDO-mVc6-Lx8eN-CTuQ&s" alt="product6" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Milk</h5>
                                         <p className="text-center"><del>$5.99</del>$4.99</p>
                                         <form className="text-center">
@@ -87,7 +87,7 @@ export function Shop() {
 
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://www.harighotra.co.uk/images/Shutterstock/tomaotoes1_560x560.jpg" className="img-fluid product-image" id="image" />
+                                        <img src="https://www.harighotra.co.uk/images/Shutterstock/tomaotoes1_560x560.jpg" alt="product7" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Tomato</h5>
                                         <p className="text-center"><del>$8.99</del>$7.99</p>
                                         <form className="text-center">
@@ -99,7 +99,7 @@ export function Shop() {
 
                                 <div className="col-lg-3 col-md-3 col-12">
                                     <div className="product-card position-relative">
-                                        <img src="https://www.veggycation.com.au/siteassets/veggycationvegetable/coriander-leaves.jpg" className="img-fluid product-image" id="image" />
+                                        <img src="https://www.veggycation.com.au/siteassets/veggycationvegetable/coriander-leaves.jpg" alt="product8" className="img-fluid product-image" id="image" />
                                         <h5 className="text-center my-3">Coriander leaves</h5>
                                         <p className="text-center"><del>$10.99</del>$9.99</p>
                                         <form className="text-center">
