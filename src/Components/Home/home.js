a83709eb5049ebbacf208c8eca09a6f0d99136bf
TharunKumar31 <tharunkumarsakthi2004@gmail.com>
2026-06-11 10:12:15 +0530

    Fix Vercel deployment issues

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,4 +1,4 @@
-import { faBasketShopping, faCartShopping, faCircleUser, faEnvelope, faSearch, faSearchLocation } from "@fortawesome/free-solid-svg-icons";
+import { faBasketShopping, faCartShopping, faCircleUser, faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
@@ -9,7 +9,7 @@ export function Home() {
             <div id="navbar">
                 <nav class="navbar navbar-expand-lg p-4">
                     <div class="container">
-                        <a class="navbar-brand" href="#" id="market"><FontAwesomeIcon icon={faBasketShopping} className="me-2" />Fresh Market</a>
+                        <a class="navbar-brand" href="#home" id="market"><FontAwesomeIcon icon={faBasketShopping} className="me-2" />Fresh Market</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
